7b47ade5bee8e4d703ccd0f52079a7bf81bf5b66
alana-gif <a.spiden@rgu.ac.uk>
2026-08-22 18:25:14 +0100

    additions

--- f1-strategy-project/pit_and_tire_analysis_ANNOTATED.py
+++ f1-strategy-project/pit_and_tire_analysis_ANNOTATED.py
@@ -146,3 +146,5 @@ if not deg_df.empty:
 pit_loss_df.to_csv('pit_loss_results.csv', index=False)
 deg_df.to_csv('tire_degradation_results.csv', index=False)
 print("\nSaved pit_loss_results.csv and tire_degradation_results.csv")
+
+
