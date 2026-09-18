89ae17a071252f2448efac0a30c38f246bad1927
alana-gif <a.spiden@rgu.ac.uk>
2026-09-18 16:13:51 +0100

    .

--- f1-strategy-project/pit_and_tire_analysis_ANNOTATED.py
+++ f1-strategy-project/pit_and_tire_analysis_ANNOTATED.py
@@ -30,8 +30,6 @@ laps = session.laps
 print(f"Loaded {len(laps)} laps from {YEAR} {GRAND_PRIX}")
 print("-" * 50)
 
-
-
 # --- PART A: PIT STOP LOSS TIME ---
 # A pit stop shows up in the data as a lap with a PitInTime and the
 # NEXT lap having a PitOutTime. We compare that lap's time to a normal
